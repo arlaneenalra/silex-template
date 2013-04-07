82b27c978c8231306e8a1b057b2d742f25062170
arlaneenalra <emeraldd.chris@gmail.com>
2013-04-06 23:32:10 -0500

    Allow automatic installation by curl or raw php.

--- composer.sh
+++ composer.sh
@@ -1,12 +1,37 @@
 #!/bin/bash
 
-INSTALL_DIR=vendor
-COMPOSER=composer.phar
+INSTALL_DIR=./vendor
+COMPOSER=${INSTALL_DIR}/composer.phar
+
+OPTS="--install-dir=${INSTALL_DIR}"
+CURL=`which curl 2> /dev/null`
+
+
+# install composer using straight php
+install_php() {
+    echo "Using php . . ."
+    php -r "eval('?>'.file_get_contents('https://getcomposer.org/installer'));" -- ${OPTS}
+}
+
+# install via curl
+install_curl() {
+    echo "Using curl . . ."
+    ${CURL} -sS https://getcomposer.org/installer | php -- ${OPTS}
+}
 
 # test for the existence of the phar before doing anything
-if [ ! -x composer.phar ];
+if [[ ! -x "${COMPOSER}" ]]
 then
-    curl -sS https://getcomposer.org/installer | php -- --install-dir=vendor
+    # try curl
+
+    if [[ -x "${CURL}" ]]
+    then
+        install_curl
+    else
+        install_php
+    fi
 fi
 
-${INSTALL_DIR}/${COMPOSER} $@
+# do an update
+${COMPOSER} self-update
+${COMPOSER} $@
